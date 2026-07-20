f40038eb2c55a3c60ad5a99e0a30f2b87c08f914
RoxyyGooberr <noivesman@gmail.com>
2026-07-19 22:09:32 -0400

    Gunshot Residue added to non-energy weapons
    GSR will now appear on a person if they fired a gun.
    
    Shoutout brodysodie again for carrying this in its entirety
    
    Co-Authored-By: SpotsDots16 <282673410+spotsdots16@users.noreply.github.com>

---
+++ Content.Server/Forensics/Systems/GunshotResidueSystem.cs
@@ -0,0 +1,29 @@
+using Content.Shared.Weapons.Ranged.Systems;
+
+namespace Content.Server.Forensics;
+
+/// <summary>
+/// Applies gunshot residue to a firearm and its user after the firearm is fired.
+/// </summary>
+public sealed class GunshotResidueSystem : EntitySystem
+{
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<GunshotResidueSourceComponent, GunShotEvent>(OnGunShot);
+    }
+
+    private void OnGunShot(
+        Entity<GunshotResidueSourceComponent> ent,
+        ref GunShotEvent args)
+    {
+        var residue = Loc.GetString(
+            "forensic-residue",
+            ("adjective", ent.Comp.ResidueAdjective));
+
+        var shooterForensics = EnsureComp<ForensicsComponent>(args.User);
+        shooterForensics.Residues.Add(residue);
+
+        var weaponForensics = EnsureComp<ForensicsComponent>(ent.Owner);
+        weaponForensics.Residues.Add(residue);
+    }
+}
